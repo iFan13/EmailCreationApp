cb768e1506db3e4200be6fcc1a507c799f258a2a
Ian Fan <ian.fan13@gmail.com>
2020-04-15 12:47:07 -0400

    Removed Todo

--- 002EmailAdminApplication/src/EmailApp/EmailApp.java
+++ 002EmailAdminApplication/src/EmailApp/EmailApp.java
@@ -3,7 +3,6 @@ package EmailApp;
 public class EmailApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Email em1 = new Email("John", "Smith");
 		System.out.println(em1.showInfo());
 	}
